b67ef37b9bf113958ddaacbb0a7bb5cece5b97e0
Anurag Pratap <anurag@joshsoftware.com>
2018-09-25 13:27:06 +0530

    Added fields in currency table

--- db/migrate/20180925072656_create_currencies.rb
+++ db/migrate/20180925072656_create_currencies.rb
@@ -1,6 +1,8 @@
 class CreateCurrencies < ActiveRecord::Migration[5.2]
   def change
     create_table :currencies do |t|
+      t.string :name
+      t.float :conversion_factor
 
       t.timestamps
     end
